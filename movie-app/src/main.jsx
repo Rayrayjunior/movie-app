31b28dc4773c4f21b866ca024bd3900f18b276f3
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-08-19 21:54:21 +0200

    Show component line removed

--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import{ createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App"
-import Shows from "./pages/Tvshows"
 
 const router = createBrowserRouter([
   {
