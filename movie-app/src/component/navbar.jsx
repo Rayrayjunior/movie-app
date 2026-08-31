29f3d4409957bcc21c678830b2ee7a6ece2b43f0
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-08-31 22:24:01 +0200

    Pages added to navbar.jsx to be displayed out

---
+++ movie-app/src/component/navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    return(
+        <nav>
+            <h2>Movie Box</h2>
+
+            <NavLink to="/">Home</NavLink>
+
+            <NavLink to="/actordetails">Actor Details</NavLink>
+
+            <NavLink to="/actors">Actors</NavLink>
+
+            <NavLink to="/animations">Animations</NavLink>
+        </nav>
+    )
+}
