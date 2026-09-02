d7ae81b42033f98fb16fab5ac5c6661f7026a743
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-09-02 23:09:04 +0200

    States added

---
+++ movie-app/src/component/Actorsdetails.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const Actors = () => {
+
+    const [actorsdata, setactordata] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useError(null);
+
+
+
+    useEffect(() => {
+
+        const loadActors = async () => {
+            
+            try {
+                setLoading
+            }
+        }
+    })
+}
