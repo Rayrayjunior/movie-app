761cbee7179a7fb26b3eadf9b5b1ca72a3b1e120
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-09-03 00:22:43 +0200

    API.js changed

--- movie-app/src/API/API.js
+++ movie-app/src/API/API.js
@@ -1,14 +1,13 @@
 const API_URL = import.meta.env.VITE_MOVIES;
 
 export const movieData = async () => {
-
-    const response = await fetch(API_URL);
+    const response = await fetch(`${API_URL}/shows`);
 
     if (!response.ok) {
-        throw new Error(`API Error: ${response.status} ${response.statusText}`);
+        throw new Error("Failed");
     }
 
     const data = await response.json();
 
     return data;
-}
+};
