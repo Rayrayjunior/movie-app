d593c77ff2b9296c016f65c009d1d6a4882c8e4f
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-09-15 22:07:53 +0200

    States added to movies component

--- movie-app/src/component/movies.jsx
+++ movie-app/src/component/movies.jsx
@@ -1,8 +1,14 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { movieData } from "../API/API";
 
 
 const Movies = () => {
+
+    const [moviesData, setMoviedata] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
     return (
         <div>
             <h1>Movies</h1>
